b9d6b17663a310f1b88da0c0b4ce48e9a92402cb
mhm301 <tus0926@naver.com>
2025-05-05 23:42:10 +0900

    setting

---
+++ src/main/java/com/ll/kit_250505/Kit250505Application.java
@@ -0,0 +1,13 @@
+package com.ll.kit_250505;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kit250505Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kit250505Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/kit_250505/Kit250505ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ll.kit_250505;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class Kit250505ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
